2fe3f7710c5dd939ba3483f6606372c20b8c7ae9
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-02-26 01:26:20 +0530

    06

--- src/test/java/selenium/S06_FileUpload.java
+++ src/test/java/selenium/S06_FileUpload.java
@@ -1,5 +1,28 @@
 package selenium;
 
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class S06_FileUpload {
+	//
+	@Test
+	public void fileUploadTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/upload");
+		File uploadFile = new File("src/test/resources/selenium-snapshot.png");
+
+		WebElement fileInput = driver.findElement(By.cssSelector("input[type=file]"));
+		fileInput.sendKeys(uploadFile.getAbsolutePath());
+		driver.findElement(By.id("file-submit")).click();
+
+		WebElement fileName = driver.findElement(By.id("uploaded-files"));
+		Assert.assertEquals("selenium-snapshot.png", fileName.getText());
+	}
 
 }
