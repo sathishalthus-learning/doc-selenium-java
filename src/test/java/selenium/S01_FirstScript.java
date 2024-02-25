8434cfc8cc272d95bec85c3b15e6becc536d7445
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-02-25 22:48:46 +0530

    02

--- src/test/java/selenium/S01_FirstScript.java
+++ src/test/java/selenium/S01_FirstScript.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 public class S01_FirstScript {
 	//
--- src/test/java/selenium/S02_FirstTest.java
+++ src/test/java/selenium/S02_FirstTest.java
@@ -14,24 +14,34 @@ public class S02_FirstTest {
 	//
 	@Test
 	public void eightComponents() {
+		
+		//1. Starting the Session
 		WebDriver driver = new ChromeDriver();
+		
+		//2. Take action on browser
 		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
+		//3. Request browser information
 		String title = driver.getTitle();
-		assertEquals("Web form", title);
+		System.out.println(title);
 
+		//4. Establish Waiting Strategy
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
+		//5. Find an element
 		WebElement textBox = driver.findElement(By.name("my-text"));
 		WebElement submitButton = driver.findElement(By.cssSelector("button"));
 
+		//6. Take action on element
 		textBox.sendKeys("Selenium");
 		submitButton.click();
+		
+		//7. Request element information 
+		String title1 = driver.getTitle();
+		System.out.println(title1);
+		assertEquals("Web form - target page", title1);
 
-		WebElement message = driver.findElement(By.id("message"));
-		String value = message.getText();
-		assertEquals("Received!", value);
-
+		//8. End the session
 		driver.quit();
 	}
 
