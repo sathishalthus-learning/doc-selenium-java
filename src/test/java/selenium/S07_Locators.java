a9f8e2717f7929fe80a208d9b5431a8b967ac19b
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-02-26 12:08:45 +0530

    07

--- src/test/java/selenium/S07_Locators.java
+++ src/test/java/selenium/S07_Locators.java
@@ -3,6 +3,7 @@ package selenium;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 
@@ -12,25 +13,45 @@ public class S07_Locators {
 	public void traditionalLocators() {
 		//
 		WebDriver driver = new ChromeDriver();
-		driver.get("file:///D:/workspaces/selenium/Locators.html");
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 		
-		driver.findElement(By.className("information"));
-		driver.findElement(By.cssSelector("#fname"));
-		driver.findElement(By.id("lname"));
-		driver.findElement(By.name("newsletter"));
-		driver.findElement(By.linkText("Selenium Official Page"));
-		driver.findElement(By.partialLinkText("Official Page"));
+		driver.findElement(By.className("form-control-color"));
+		driver.findElement(By.cssSelector("#my-text-id"));
+		driver.findElement(By.id("my-text-id"));
+		driver.findElement(By.name("my-text"));
+		driver.findElement(By.linkText("Return to index"));
+		driver.findElement(By.partialLinkText("Return"));
 		driver.findElement(By.tagName("a"));
-		driver.findElement(By.xpath("//input[@value='f']"));
+		driver.findElement(By.xpath("//input[@type='color']"));
+		
+		driver.quit();
 	}
 
+	//
+	@Test
 	public void relativeLocators() {
 		//
-		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+		//
+		WebElement textInputField = driver.findElement(RelativeLocator.with(By.name("my-text")).above(By.name("my-password")));
+		textInputField.sendKeys("hello");
+		driver.quit();
 	}
 	
+	//
+	@Test
 	public void chainingLocators() {
 		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+		//
+		WebElement textInputField = driver.findElement(
+				RelativeLocator.with(By.name("my-text"))
+				.above(By.name("my-password"))
+				.near(By.name("my-textarea")));
+		textInputField.sendKeys("hello");
 		
+		driver.quit();
 	}
 }
