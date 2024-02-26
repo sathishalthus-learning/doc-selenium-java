2c59b2f111f5919d9b875e48039e82f8a7ed665b
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-02-26 23:45:03 +0530

    10

--- src/test/java/selenium/S10_InformationAboutElements.java
+++ src/test/java/selenium/S10_InformationAboutElements.java
@@ -6,62 +6,122 @@ import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class S10_InformationAboutElements {
-	
-	WebDriver driver = new ChromeDriver();
-	driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-    // Navigate to Url
-    driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+	//
+	@Test
+	public void isDisplayed() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// isDisplayed
+		// Get boolean value for is element display
+		boolean isEmailVisible = driver.findElement(By.name("email_input")).isDisplayed();
+		assertEquals(isEmailVisible, true);
+		//
+		driver.quit();
+	}
 
-	// isDisplayed        
-   // Get boolean value for is element display
-   boolean isEmailVisible = driver.findElement(By.name("email_input")).isDisplayed();
-   assertTrue(isEmailVisible,true);
+	//
+	@Test
+	public void isEnabled() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// isEnabled
+		// returns true if element is enabled else returns false
+		boolean isEnabledButton = driver.findElement(By.name("button_input")).isEnabled();
+		assertEquals(isEnabledButton, true);
+		//
+		driver.quit();
+	}
 
-   //isEnabled
-  //returns true if element is enabled else returns false
-   boolean isEnabledButton = driver.findElement(By.name("button_input")).isEnabled();
-   assertEquals(isEnabledButton,true);
+	//
+	@Test
+	public void isSelected() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// isSelected
+		// returns true if element is checked else returns false
+		boolean isSelectedCheck = driver.findElement(By.name("checkbox_input")).isSelected();
+		assertEquals(isSelectedCheck, true);
+		//
+		driver.quit();
+	}
 
-   //isSelected
-   //returns true if element is checked else returns false
-   boolean isSelectedCheck = driver.findElement(By.name("checkbox_input")).isSelected();
-   assertEquals(isSelectedCheck,true); 
+	//
+	@Test
+	public void getTagName() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// TagName
+		// returns TagName of the element
+		String tagNameInp = driver.findElement(By.name("email_input")).getTagName();
+		assertEquals(tagNameInp, "input");
+		//
+		driver.quit();
+	}
 
-   //TagName
-   //returns TagName of the element
-   String tagNameInp = driver.findElement(By.name("email_input")).getTagName();
-   assertEquals(tagNameInp,"input"); 
+	//
+	@Test
+	public void getSizeAndPosition() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// GetRect
+		// Returns height, width, x and y coordinates referenced element
+		Rectangle res = driver.findElement(By.name("range_input")).getRect();
+		// Rectangle class provides getX,getY, getWidth, getHeight methods
+		assertEquals(res.getX(), 10);
+		//
+		driver.quit();
+	}
 
-   //GetRect
-   // Returns height, width, x and y coordinates referenced element
-   Rectangle res =  driver.findElement(By.name("range_input")).getRect();
-   // Rectangle class provides getX,getY, getWidth, getHeight methods
-   assertEquals(res.getX(),10);
-   
+	//
+	@Test
+	public void getCSSValue() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// Retrieves the computed style property 'font-size' of field
+		String cssValue = driver.findElement(By.name("color_input")).getCssValue("font-size");
+		assertEquals(cssValue, "13.3333px");
+		//
+		driver.quit();
+	}
 
-// Retrieves the computed style property 'font-size' of field
-String cssValue = driver.findElement(By.name("color_input")).getCssValue("font-size");
-assertEquals(cssValue, "13.3333px");
-   
-   
-   //GetText
-  // Retrieves the text of the element
-   String text = driver.findElement(By.tagName("h1")).getText();
-   assertEquals(text, "Testing Inputs");
-   
-   
-   //FetchAttributes
- //identify the email text box
- WebElement emailTxt = driver.findElement(By.name(("email_input")));
-//fetch the value property associated with the textbox
- String valueInfo = emailTxt.getAttribute("value");
- assertEquals(valueInfo,"admin@localhost");
-   
-   
-   driver.quit();
-}
+	//
+	@Test
+	public void getTextContent() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// GetText
+		// Retrieves the text of the element
+		String text = driver.findElement(By.tagName("h1")).getText();
+		assertEquals(text, "Testing Inputs");
+		//
+		driver.quit();
+	}
 
+	//
+	@Test
+	public void getAttributesAndProperties() {
+		//
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		// FetchAttributes
+		// identify the email text box
+		WebElement emailTxt = driver.findElement(By.name(("email_input")));
+		// fetch the value property associated with the textbox
+		String valueInfo = emailTxt.getAttribute("value");
+		assertEquals(valueInfo, "admin@localhost");
+		//
+		driver.quit();
+	}
 }
