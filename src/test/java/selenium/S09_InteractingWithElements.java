acf213a76420fb61002cd1535e5f73ab22a25b0a
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-02-26 23:34:01 +0530

    09

--- src/test/java/selenium/S09_InteractingWithElements.java
+++ src/test/java/selenium/S09_InteractingWithElements.java
@@ -7,40 +7,41 @@ import java.time.Duration;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class S09_InteractingWithElements {
 	//
-	 @Test
-	    public void interactWithElements() {
-	        WebDriver driver = new ChromeDriver();
-	        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-	        // Navigate to Url
-	        driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+	@Test
+	public void interactWithElements() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+		// Navigate to Url
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
 
-		    // Click on the element 
-	        WebElement checkInput=driver.findElement(By.name("checkbox_input"));
-	        checkInput.click();
-	        Boolean isChecked=checkInput.isSelected();
-	        assertEquals(isChecked,false);
+		// Click on the element
+		WebElement checkInput = driver.findElement(By.name("checkbox_input"));
+		checkInput.click();
+		Boolean isChecked = checkInput.isSelected();
+		assertEquals(isChecked, false);
 
-	        //SendKeys
-	        // Clear field to empty it from any previous data
-	        WebElement emailInput=driver.findElement(By.name("email_input"));
-	        emailInput.clear();
-		    //Enter Text
-	        String email="admin@localhost.dev";
-		    emailInput.sendKeys(email);
-	        //Verify
-	        String data=emailInput.getAttribute("value");
-	        assertEquals(data,email);
+		// SendKeys
+		// Clear field to empty it from any previous data
+		WebElement emailInput = driver.findElement(By.name("email_input"));
+		emailInput.clear();
+		// Enter Text
+		String email = "admin@localhost.dev";
+		emailInput.sendKeys(email);
+		// Verify
+		String data = emailInput.getAttribute("value");
+		assertEquals(data, email);
 
+		// Clear Element
+		// Clear field to empty it from any previous data
+		emailInput.clear();
+		data = emailInput.getAttribute("value");
+		assertEquals(data, "");
 
-	        //Clear Element
-	        // Clear field to empty it from any previous data
-	        emailInput.clear();
-	        data=emailInput.getAttribute("value");
-	        assertEquals(data, ""); 
-
-	        driver.quit();
+		driver.quit();
+	}
 }
