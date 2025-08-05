335a94cce11ac163c08c8cefd762d500fd3b504a
sathishalthus-learning <sathishalthus.learning@gmail.com>
2025-08-05 16:57:06 +0530

    Changes in Replaceall

--- src/test/java/playground/StringReplace.java
+++ src/test/java/playground/StringReplace.java
@@ -4,6 +4,7 @@ public class StringReplace {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
+		//Learning replaceAll
 		String s="Rs.1900.50";
 		String modifiedString = s.replaceAll("^[^\\d]+","");
 		System.out.println(modifiedString);
