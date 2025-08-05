efec9cee86005f0f06ddbae22749ceec48166328
sathishalthus-learning <140003262+sathishalthus-learning@users.noreply.github.com>
2025-08-05 17:01:03 +0530

    Update StringReplace.java

--- src/test/java/playground/StringReplace.java
+++ src/test/java/playground/StringReplace.java
@@ -3,7 +3,8 @@ package playground;
 public class StringReplace {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+		// TODO Auto-generated method stub
+		//learning github merge
 		//Learning replaceAll
 		String s="Rs.1900.50";
 		String modifiedString = s.replaceAll("^[^\\d]+","");
