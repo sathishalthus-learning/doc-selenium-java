195eddd4c1f30279b6e55a3534ddff3fcf2bf7a8
sathishalthus-learning <140003262+sathishalthus-learning@users.noreply.github.com>
2025-08-05 16:56:13 +0530

    Update AutomationTestingIo.java

--- src/test/java/samples/AutomationTestingIo.java
+++ src/test/java/samples/AutomationTestingIo.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutomationTestingIo {
 
 	public static void main(String[] args) {
-		// 
+		//learning git 
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
